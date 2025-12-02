41a949f1f3749e2cf91ad7883ea1b5d604b77b9a
JersonNetcol <j.espitia@netcol.net.co>
2025-12-02 09:58:15 -0500

    2/12/2025

--- entradas-salidas/next.config.ts
+++ entradas-salidas/next.config.ts
@@ -1,11 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  turbopack: {
-    root: "./",
-  },
+  output: "standalone",
 };
 
 export default nextConfig;
